efd6926b154e316721c4c569ccb0b39ba35ab5df
rilesdun <rilydunlap@outlook.com>
2023-08-13 03:58:28 +0000

    inital setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./**/*.html', './**/*.vue', './**/*.jsx'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
