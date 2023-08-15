0a6c7848c57b4054770141fce4b24eb0ac380159
Rily Dunlap <rilydunlap@outlook.com>
2023-08-15 15:15:23 -0300

    refactored current supply

--- static/js/currentSupply.js
+++ static/js/currentSupply.js
@@ -1,13 +1,11 @@
 function updateCurrentSupply() {
-  fetch("http://localhost:5000/api/supply") // Make sure the URL is correct
-    .then((response) => response.json())
-    .then((data) => {
-      const supply = data.circulating;
-      const formattedSupply = `${parseFloat(supply).toFixed(2)}`;
+  fetch("http://localhost:5000/api/circulating_supply") // Make sure the URL is correct
+    .then((response) => response.text()) // Handle the response as text
+    .then((supply) => {
+      const formattedSupply = `${parseFloat(supply).toFixed(2)}`; // Parse the text as a float
       document.querySelector(".current-supply span").textContent =
         formattedSupply;
     })
-
     .catch((error) => {
       console.error("Error fetching current supply:", error);
     });
