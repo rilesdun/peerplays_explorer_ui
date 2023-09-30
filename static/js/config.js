afcfb2397feacd847eab29b654667ee3730a4b4b
rilesdun <85508491+rilesdun@users.noreply.github.com>
2023-09-30 12:10:20 -0300

    fix: duplicated / in api url

--- static/js/config.js
+++ static/js/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: "https://explorer.rilydunlap.me/", 
-  // update to your IP of your server ^^
+  BASE_URL: "https://explorer.rilydunlap.me", 
+  // update to you accordingly to your backend location
   
 };
